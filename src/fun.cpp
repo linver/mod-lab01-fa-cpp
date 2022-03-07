9553d0cf77d695cc5fc22551aa18ec326d21e5a2
Alina Overina <alinaoverina@gmail.com>
2022-03-07 15:31:29 +0300

    second attempt

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,77 +1,68 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <math.h>
 #include <cctype>
-#include <math.h>   
+
 
 unsigned int faStr1(const char *str) {
-    // Разработать функцию на языке С++, которая в переданной строке определяет слова, не содержащие цифр и подсчитывает количество таких слов.
     int words_amount = 0;
     bool separator = false;
     int i = 0;
-    while (str[i])
-    {
-        if(separator == false && str[i] != ' ' && isalpha(str[i]))
-        {
+    while (str[i]) {
+        if (separator == false && str[i] != ' ' && isalpha(str[i])) {
             separator = true;
             words_amount++;
-        }
-        else if (separator == true && str[i] == ' ')
+        } else if (separator == true && str[i] == ' ') {
             separator = false;
+        }
         i++;
     }
     return words_amount;
 }
 
-unsigned int faStr2(const char *str) { 
-    // Разработать функцию на языке С++, которая в переданной строке подсчитывает количество слов, 
-    // начинающиеся на заглавную латинскую букву, а среди других символов этих слов только латинские строчные буквы.
+unsigned int faStr2(const char *str) {
     int words_amount = 0;
     bool separator = false;
     int i = 0;
-    while (str[i])
-    {
-        if(separator == false && str[i] != ' ' && isalpha(str[i]) && str[i] == toupper(str[i]) && str[i] >= 'A' && str[i] <='Z')
-        {
+    while (str[i]) {
+        if (separator == false && str[i] != ' ' && isalpha(str[i])
+        && str[i] == toupper(str[i]) && str[i] >= 'A' && str[i] <='Z') {
             int k = i;
             int all_chars = i;
-            while (str[k] != ' ')
-            {
-                if (str[k] == tolower(str[k]) && str[i] >= 'a' && str[i] <='z')
-                {
+            while (str[k] != ' ') {
+                if (str[k] == tolower(str[k])
+                && str[i] >= 'a' && str[i] <='z') {
                     k++;
                 }
                 all_chars++;
             }
-            if (all_chars == k)
-            {
+            if (all_chars == k) {
                 separator = true;
                 words_amount++;
             }
-        }
-        else if (separator == true && str[i] == ' ')
+        } else if (separator == true && str[i] == ' ') {
             separator = false;
+        }
         i++;
     }
     return words_amount;
 }
 
 unsigned int faStr3(const char *str) {
-    // Разработать функцию на языке С++, которая находит среднюю длину слова в строке, округляя ее до целого значения по правилам округления.
     int words_amount = 0;
     bool separator = false;
     int i = 0;
     int all_chars = 0;
-    while (str[i])
-    {
-        if(separator == false && str[i] != ' ')
-        {
+    while (str[i]) {
+        if (separator == false && str[i] != ' ') {
             separator = true;
             words_amount++;
-        }
-        else if (separator == true && str[i] == ' ')
+        } else if (separator == true && str[i] == ' ') {
             separator = false;
-        if (str[i] != ' ')
+        }
+        if (str[i] != ' ') {
             all_chars++;
+        }
         i++;
     }
     int average_word_length = round(words_amount/all_chars);
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
 // Copyright 2022 UNN-IASR
-#include <iostream>
-#include "fun.h" 
+#include "fun.h"
 
 
 int main() {
-    using std::cout;
-    const char* string = "My favorite programming language is Python3";
-    cout << faStr1(string);
-    cout << faStr2(string);
-    cout << faStr3(string);
     return 0;
 }
